d57af8b3384ecb22e265438fd3f7c46b2d6f4721
Wouter van Atteveldt <wouter@vanatteveldt.com>
2017-02-11 01:05:47 +0800

    First checkin dockerfile and compose

---
+++ amcat/etc/create_superuser.py
@@ -0,0 +1,12 @@
+from django.contrib.auth.models import User;
+
+username = 'amcat';
+password = 'amcat';
+email = 'amcat@example.com';
+
+if User.objects.filter(username=username).count()==0:
+    User.objects.create_superuser(username, email, password)
+    print('Superuser {username!r} created.'.format(**locals()))
+else:
+    print('User {username!r} already existed .')
+
