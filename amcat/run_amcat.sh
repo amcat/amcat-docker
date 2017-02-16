4db7e6de1b3bcd8d98654ec785957d1f7f2a730b
Wouter van Atteveldt <wouter@vanatteveldt.com>
2017-02-16 17:43:23 +0800

    Remove reference to /etc/amcat path for scripts

--- amcat/run_amcat.sh
+++ amcat/run_amcat.sh
@@ -1,9 +1,9 @@
 # wait for elastic and postgres
 set -e
-python3 wait_services.py
+python3 /wait_services.py
 if ! (psql -h postgres -U postgres -lqtA | grep -q "^amcat|"); then
     createdb -h postgres -U postgres amcat
 fi
 python3 -m amcat.manage migrate
-python3 -m amcat.manage shell < /etc/amcat/create_superuser.py
+python3 -m amcat.manage shell < /create_superuser.py
 python3 -m amcat.manage runserver 0.0.0.0:8000
